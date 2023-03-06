71c220bf646d632944d131f69ed73cdf4ff374d4
s-121-29 <s-121-29@gumiforest.ae.krakow.pl>
2023-03-06 19:48:32 +0100

    initial for ecommerce

---
+++ src/main/java/ksikora/App.java
@@ -0,0 +1,38 @@
+package ksikora;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jakub","Michał","Agnieszka","Kasia");
+        String[] namesAsArray = {"Jakub"};
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Jakub"); // Hello Jakub
+
+        List<String> ladies = new ArrayList<String>();
+
+        for (String name: names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+
+        for (String ladyName: ladies) {
+            greeter.greet(ladyName);
+        }
+
+        names.stream()
+                .filter(name -> name.endsWith("a"))
+                .filter(name -> name.startsWith("A"))
+                .map(String::toUpperCase)
+                .forEach(greeter::greet);
+
+        //greet just ladies
+        //Hello Agnieszka
+        //Hello Kasia
+
+    }
+}
